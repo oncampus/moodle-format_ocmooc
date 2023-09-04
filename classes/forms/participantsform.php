fcaddcd0de72ff883677f6372712b1455848767a
Laurenz Schindler <Laurenz.Schindler@oncampus.de>
2023-09-04 10:10:46 +0200

    Added language strings | removed core duplicates | quickfix participants.php import line 11

--- classes/forms/participantsform.php
+++ classes/forms/participantsform.php
@@ -9,36 +9,36 @@ class participantsform extends \moodleform {
     protected function definition() {
         $form = $this->_form;
 
-        $form->addElement('checkbox', 'profilepicture', get_string('profilepicture', 'format_ocmooc'));
+        $form->addElement('checkbox', 'profilepicture', get_string('userpic'));
         $form->setDefault('profilepicture', true);
 
         $options = [
-                0 => get_string('fullname', 'format_ocmooc'),
-                1 => get_string('username', 'format_ocmooc'),
+                0 => get_string('fullname'),
+                1 => get_string('username'),
                 2 => get_string('anonymized', 'format_ocmooc'),
         ];
         $form->addElement('select', 'namedisplay', get_string('namedisplay', 'format_ocmooc'), $options);
         $form->setDefault('namedisplay', 0);
 
-        $form->addElement('checkbox', 'email', get_string('email', 'format_ocmooc'));
+        $form->addElement('checkbox', 'email', get_string('email'));
         $form->setDefault('email', true);
 
-        $form->addElement('checkbox', 'town', get_string('town', 'format_ocmooc'));
+        $form->addElement('checkbox', 'town', get_string('city'));
         $form->setDefault('town', true);
 
-        $form->addElement('checkbox', 'country', get_string('country', 'format_ocmooc'));
+        $form->addElement('checkbox', 'country', get_string('country'));
         $form->setDefault('country', true);
 
-        $form->addElement('checkbox', 'badges', get_string('badges', 'format_ocmooc'));
+        $form->addElement('checkbox', 'badges', get_string('badges'));
         $form->setDefault('badges', true);
 
-        $form->addElement('checkbox', 'roles', get_string('roles', 'format_ocmooc'));
+        $form->addElement('checkbox', 'roles', get_string('roles'));
         $form->setDefault('roles', false);
 
-        $form->addElement('checkbox', 'groups', get_string('groups', 'format_ocmooc'));
+        $form->addElement('checkbox', 'groups', get_string('groups'));
         $form->setDefault('groups', false);
 
-        $form->addElement('checkbox', 'lastaccess', get_string('lastaccess', 'format_ocmooc'));
+        $form->addElement('checkbox', 'lastaccess', get_string('lastaccess'));
         $form->setDefault('lastaccess', false);
 
         $this->add_action_buttons();
