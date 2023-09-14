826234997710d8a98d9bed4939c4c8e4e0d0fb96
Laurenz Schindler <Laurenz.Schindler@oncampus.de>
2023-09-14 11:19:30 +0200

    hvp element fix mod_hvp changed

--- format.php
+++ format.php
@@ -51,3 +51,4 @@ echo $renderer->render($widget);
 
 // Include any format js module here using $PAGE->requires->js.
 $PAGE->requires->js('/course/format/ocmooc/chapterslider.js');
+$PAGE->requires->js('/course/format/ocmooc/amd/build/hvp_resizer_parent.min.js',true);
