2f93b951c9f24bcd65865867955cb67261d8f12f
Jordan <jordan.krause@oncampus.de>
2024-01-31 12:23:05 +0100

    Fixed h5p Issues: MMK-4,5,6,10,11

--- amd/src/hvp_resizer_child.js
+++ amd/src/hvp_resizer_child.js
@@ -12,46 +12,61 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-define(['jquery', 'format_ocmooc/hvp_resizer_child'], function($) {
-        /**
-         * Sends a post message to the parent window
-         */
-        function sendPostMessage() {
-            let queryString = window.location.search;
-            let urlParams = new URLSearchParams(queryString);
-            let id = urlParams.get('id');
-            let iFrame = document.querySelector('.h5p-iframe');
-            window.parent.postMessage({
-                frameHeight: iFrame.clientHeight,
-                frameId: 'h5p-iframe-' + id
-            });
-        }
+define(['jquery', 'format_ocmooc/hvp_resizer_child'], function ($) {
+    /**
+     * Sends a post message to the parent window
+     */
+    function sendPostMessage(contentHeight) {
+        let queryString = window.location.search;
+        let urlParams = new URLSearchParams(queryString);
+        let id = urlParams.get('id');
+        window.parent.postMessage({
+            frameHeight: contentHeight,
+            frameId: 'h5p-iframe-' + id
+        });
+    }
 
-        return {
-            handleFrame: function() {
-                // Wait for document
-                // Options for the observer (which mutations to observe)
-                const config = {attributes: true, childList: false, subtree: false};
-                // Callback function to execute when mutations are observed
-                const callback = (mutationList) => {
-                    for (const mutation of mutationList) {
-                        if (mutation.type === "attributes") {
-                            sendPostMessage();
-                        }
+    return {
+        handleFrame: function () {
+            // Wait for document
+            // Options for the observer (which mutations to observe)
+            const config = { attributes: true, childList: false, subtree: false };
+            // Callback function to execute when mutations are observed
+            const callback = (mutationList) => {
+                for (const mutation of mutationList) {
+                    if (mutation.type === "attributes") {
+                        let iFrame = document.querySelector('.h5p-iframe');
+
+                        sendPostMessage(iFrame.clientHeight);
                     }
-                };
-                const observer = new MutationObserver(callback);
+                }
+            };
+            const observer = new MutationObserver(callback);
+
+
+            //Check if a iframe Element exists
+            if ($('.h5p-iframe').length > 0) {
                 // Wait for iframe
-                $('.h5p-iframe').ready(function() {
+                $('.h5p-iframe').ready(function () {
                     let iFrame = document.querySelector('.h5p-iframe');
                     if (iFrame) {
                         if (iFrame.classList.contains('h5p-initialized')) {
-                            sendPostMessage();
+                            sendPostMessage(iFrame.clientHeight);
                         }
                         observer.observe(iFrame, config);
                     }
                 });
-            },
-        };
-    }
+            } else {
+                // Select the element with the class .h5p-content
+                var h5pContent = $('.h5p-content');
+                //Wait for H5P-Element
+                $('.h5p-content').ready(function () {
+                    if(h5pContent){
+                        sendPostMessage(h5pContent.parent().height());
+                    }
+                });
+            }
+        },
+    };
+}
 );
