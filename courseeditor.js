674664c4b9e206b5d5da669bddcec3bf35c5eb4b
Jordan <jordan.krause@oncampus.de>
2024-02-02 15:03:32 +0100

    Fixed MMK-7 CollapseNotWorking

---
+++ courseeditor.js
@@ -0,0 +1,30 @@
+/**
+ * Function called to toggle the collapse <span> Elements (Collapse All / Expand All)
+ * Example call in: templates/local/content/section/content.mustache
+ */
+function toggleCollapse() {
+    let collapseSection = document.getElementById('collapsesections');
+    //Check if collapse Section Button is expanded
+    let isExpanded = collapseSection.getAttribute('aria-expanded') === 'true';
+    //The collapse all span Element
+    let collapseSpan = document.querySelector('.collapseall');
+    //The Expand All Span Element
+    let expandSpan = document.querySelector('.expandall');
+    //The Content Body Element which gets collapsed
+    let contentElement = document.querySelector('.content.course-content-item-content.collapse');
+
+    //Toggle and set the isExpanded Status
+    isExpanded = !isExpanded;
+    collapseSection.setAttribute('aria-expanded', isExpanded);
+
+    //If new CollapseSection is expanded show the new span and body
+    if (isExpanded) {
+        collapseSpan.style.display = 'inline';
+        expandSpan.style.display = 'none';
+        contentElement.classList.add('show');
+    } else {
+        collapseSpan.style.display = 'none';
+        expandSpan.style.display = 'inline';
+        contentElement.classList.remove('show');
+    }
+}
