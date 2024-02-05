32de859fc6ba40b285fe68ce59d3ecbef1ba3344
Jordan Krause <jordan.krause@oncampus.de>
2024-02-05 12:34:14 +0000

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_ocmooc';
 $plugin->release = '0.1.5';
-$plugin->version = 2023113000;
+$plugin->version = 2024020500;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
