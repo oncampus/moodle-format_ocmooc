94e842fa451c5ce5d88c5d520696c99c8eea20c0
Marius Rosenbaum <marius.rosenbaum@oncampus.de>
2024-03-06 14:21:47 +0100

    Ãfixed variable name being wrong

--- classes/badges.php
+++ classes/badges.php
@@ -167,7 +167,7 @@ class badges extends base {
             }
         }
         //If no Certificate is available hide the Certificate Header
-        if(!$certificate_m && !$ildcert_m && !$simple_cert){
+        if(!$certificate_m && !$ildcert_m && !$simplecert_m){
             echo \html_writer::tag('div', \html_writer::tag('div', get_string('cert_nocert', 'format_ocmooc'), array('class' => 'oc_badges_text')));
         }
         echo \html_writer::end_div();
