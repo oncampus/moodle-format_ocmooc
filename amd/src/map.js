e7751df5a6fd9dc0c3894e790483230212d2b886
Jonas Reuter <jonas.reuter@oncampus.de>
2025-01-28 13:39:03 +0100

    MMK-33: Add leaflet markercluster

--- amd/src/map.js
+++ amd/src/map.js
@@ -3,46 +3,52 @@ import {call as fetchMany} from 'core/ajax';
 import {get_string as getString} from 'core/str';
 
 export const init = async (courseid) => {
-
     const overlay = document.getElementById('map-overlay');
-    const map = L.map('map', {preferCanvas: true}).setView([51.1657, 10.4515], 5);
+    const map = L.map('map', { preferCanvas: true }).setView([51.1657, 10.4515], 5);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    // Cluster Layer erstellen
+    const markersCluster = L.markerClusterGroup();
+
     try {
-        // Fetch data
+        // Daten abrufen
         const response = await fetchParticipantLocations(courseid);
 
-        // Remove overlay after data is fetched
+        // Entferne Overlay nach dem Laden der Daten
         if (overlay) {
             overlay.style.transition = 'opacity 0.3s';
             overlay.style.opacity = '0';
             setTimeout(() => overlay.remove(), 300);
         }
 
-        // Process response and add markers
+        // Teilnehmerdaten verarbeiten
         const markersWithStrings = await Promise.all(response.locations.map(async (marker) => {
             const participantString = marker.participant_count === 1
                 ? await getString('participant', 'format_ocmooc')
                 : await getString('participants', 'core');
 
-            return {
-                ...marker,
-                participantString
-            };
+            return { ...marker, participantString };
         }));
 
+        // Marker mit Clustering hinzufügen
         markersWithStrings.forEach((marker) => {
             const popupContent = `<b>${marker.location_name}</b><br>${marker.participant_count} ${marker.participantString}`;
 
-            L.marker([marker.latitude, marker.longitude], {
+            const markerInstance = L.marker([marker.latitude, marker.longitude], {
                 alt: `${marker.location_name}: ${marker.participant_count} ${marker.participantString}`
-            }).addTo(map)
-                .bindPopup(popupContent);
+            }).bindPopup(popupContent);
+
+            markersCluster.addLayer(markerInstance);
         });
+
+        // Cluster zur Karte hinzufügen
+        map.addLayer(markersCluster);
+
     } catch (error) {
         console.error('Failed to fetch participant locations:', error);
-        // Optionally, handle the error and provide feedback to the user
     }
 };
 
